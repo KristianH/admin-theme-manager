fd6454f73c7053fab8b8f11d8f4e4ce068654e5d
KristianHempel <kh@shopmodule.com>
2019-02-15 09:23:47 +0100

    added metadata.php and picture.png as QR code

---
+++ src/metadata.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.0';
+
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'             => 'admin-theme-manager',
+    'title'          => 'Admin Theme Manager',
+    'description'    => array(
+        'de' => '',
+        'en' => '',
+    ),
+    'thumbnail'      => 'picture.png',
+    'version'        => '0.0.1',
+    'author'         => 'https://github.com/KristianH & https://github.com/vanilla-thunder/',
+    'url'            => 'https://github.com/KristianH/admin-theme-manager/',
+    'email'          => '',
+    'extend'         => array(
+//        \OxidEsales\Eshop\Core\Config::class  => stdClass::class,
+    ),
+    'controllers'   => array(
+//        'controllerMapName'    => stdClass::class,
+    ),
+    'templates'      => array(
+//        'template.tpl' => 'pathtotemplate.tpl',
+    ),
+    'events'         => array(
+//        'onActivate' => stdClass::class . '::method',
+//        'onDeactivate' => stdClass::class . '::method',
+    ),
+    'blocks'         => array(
+//        array(
+//            'template' => '',
+//            'block'    => '',
+//            'file'     => '',
+//        ),
+    ),
+    'settings' => array(
+//        array(
+//            'group'     => '',
+//            'name'      => '',
+//            'type'      => '',
+//            'value'     => ''
+//        ),
+    ),
+);
+
